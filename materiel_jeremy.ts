e8da856ecb19278c58a4ddf3c87451e57b8d34d6
JCrache <jeremy.roquin.scse@gmail.com>
2018-09-06 17:30:35 +0200

    3eme

--- materiel_jeremy.ts
+++ materiel_jeremy.ts
@@ -3,3 +3,9 @@ function validate(){
 	document.getElementById("design").innerHTML = designation;
 } 
 
+let list = document.querySelector( '[role="tablist"]' );
+let tablist = new window.Tablist( list );
+
+
+// démarage du plugin
+tablist.mount();
