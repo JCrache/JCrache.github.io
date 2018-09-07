503fbf7d5a316a712c49d8d677932e71ca580b79
JCrache <jeremy.roquin.scse@gmail.com>
2018-09-07 10:27:39 +0200

    details remplace tablist

--- materiel_jeremy.ts
+++ materiel_jeremy.ts
@@ -2,14 +2,3 @@ function validate(){
 	let designation = document.getElementById("designation").value;
 	document.getElementById("design").innerHTML = designation;
 } 
-
-let list = document.querySelector( '[role="tablist"]' );
-
-Array.prototype.forEach.call( lists, function( list ) {
-  new window.Tablist( list ).mount();
-});
-
-let tablist = new window.Tablist( list );
-
-// démarage du plugin
-tablist.mount();
