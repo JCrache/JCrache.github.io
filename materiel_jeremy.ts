5e1df8900c1d02fd5b4a6942a3823a2f8eba5571
JCrache <jeremy.roquin.scse@gmail.com>
2018-09-07 17:40:16 +0200

    couleur

--- materiel_jeremy.ts
+++ materiel_jeremy.ts
@@ -2,3 +2,47 @@ function validate(){
 	let designation = document.getElementById("designation").value;
 	document.getElementById("design").innerHTML = designation;
 } 
+
+function autocolor(){
+	
+	let couleur = "#";
+	
+	let i;
+	for (i = 0; i < 6; i++){
+		let a = 2+Math.floor(14*Math.random());
+		a = a.toString();
+		switch(a){
+			case "10":
+				a = "A";
+				break;
+			case "11":
+				a = "B";
+				break;
+			case "12":
+				a = "C";
+				break;
+			case "13":
+				a = "D";
+				break;
+			case "14":
+				a = "E";
+				break;
+			case "15":
+				a = "F";
+				break;
+		}
+		couleur = couleur+a;
+	}
+	
+	let corps = document.getElementById("body");
+	corps.style.backgroundColor = couleur;
+	
+	return;
+}
+
+function changecolor(){
+	
+	let intervalID;
+	intervalID = setInterval(autocolor,50);
+	
+}
